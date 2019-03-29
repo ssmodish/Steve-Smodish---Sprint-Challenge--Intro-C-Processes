abdcec03edfcba8f1d9ffb88ea2255ddf33aa936
Smodi <stevesmodish@gmail.com>
2019-03-29 12:45:18 -0400

    Appears to be iterating through the directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,27 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *entry;
   // Parse command line
+  /* if there are no extra arguements show contents of current directory
+      otherwise show contents of given directory */
+  if(argc == 1){
+    argv[1] = ".";
+  }
 
   // Open directory
+  dir = opendir(argv[1]);
 
   // Repeatly read and print entries
+  while((entry = readdir(dir)) != NULL)
+  {
+    printf("Reading directory item\n");
+  }
+
+
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
