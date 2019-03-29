6214752a7fa2727ad9f18e49ed1aaee033b7d9c2
Smodi <stevesmodish@gmail.com>
2019-03-29 13:36:55 -0400

    Almost there, just have to get sizes.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 #include <sys/stat.h>
 
 /**
@@ -10,26 +11,33 @@ int main(int argc, char **argv)
   DIR *dir;
   struct dirent *entry;
   struct stat buf;
+  char path[128], f_name[128], fullpath[256];
 
   // Parse command line
   /* if there are no extra arguements show contents of current directory
       otherwise show contents of given directory */
   if(argc == 1){
-    argv[1] = ".";
+    strcpy(path, ".");
+  } else {
+    strcpy(path, argv[1]);
   }
 
   // Open directory
-  dir = opendir(argv[1]);
+  dir = opendir(path);
 
-  printf("Reading directory %s\n", argv[1]);
+  printf("Reading directory %s\n", path);
 
   // Repeatly read and print entries
   while((entry = readdir(dir)) != NULL)
   {
-    char *f_name = entry->d_name;
-    char *fullpath = argv[1] + "/" + f_name; // need to be properly concatenated
+    fullpath[0] = '\0';
+    strcpy(f_name, entry->d_name);
+    strcat(fullpath, path);
+    strcat(fullpath, "/");
+    strcat(fullpath, f_name);
+    // printf("%s\n", fullpath);
     int f_size = stat(fullpath, &buf);
-    printf("%10lld %s\n", buf.st_size, f_name);
+    printf("%10d %s\n", f_size, f_name);
   }
 
 
